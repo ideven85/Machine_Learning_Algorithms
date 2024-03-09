84e957875e88592fd345b5c67e5e5b675890b76f
Deven Kalra <deven@Macbook.local>
2024-03-09 14:48:54 +0530

    Added Files Through Upload

--- Algorithms/Hashing.py
+++ Algorithms/Hashing.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 def zigzagTraverse(array):
     # Write your code here.
     n = len(array)
@@ -22,6 +25,21 @@ def zigzagTraverse(array):
         total+=1
     return output
 
+
+
+def checkIfPangram( sentence: str) -> bool:
+    n = len(sentence)
+    if n<26:
+        return False
+    mapping = [False for _ in range(26)]
+    for char in sentence:
+        mapping[ord(char)-ord('a')] = True
+    return all(mapping)
+
+def missingNumber( nums: List[int]) -> int:
+    n = len(nums)
+    return (n*(n+1))//2 - sum(nums)
+
 if __name__ == '__main__':
     matrix = [[1,3,4,10],
               [2,5,9,11],
@@ -29,4 +47,7 @@ if __name__ == '__main__':
               [7,13,14,16]
               ]
     print(zigzagTraverse(matrix))
-
+    sentence = "thequickbrownfoxjumpsoverthelazydog"
+    print(checkIfPangram(sentence))
+    sentence1 = "leetcode"
+    print(checkIfPangram(sentence1))
---
+++ Design_Patterns/strategy/Order.py
@@ -0,0 +1,58 @@
+from abc import ABC, abstractmethod
+from collections.abc import Sequence
+from decimal import Decimal
+from typing import NamedTuple, Optional
+
+#todo Revisit OOPS in Python
+class Customer(NamedTuple):
+    name: str
+    fidelity: int
+
+class LineItem(NamedTuple):
+    product: str
+    price: float
+    quantity: int
+
+    def total(self):
+        return self.price * self.quantity
+
+class Order(NamedTuple):
+    customer: Customer
+    cart: Sequence[LineItem]
+    promotion: Optional['Promotion']=None
+
+    def total(self):
+        totals = (item.total() for item in self.cart)
+        return sum(totals,start=Decimal(0))
+
+    def due(self):
+        if not self.promotion:
+            discount = Decimal(0)
+        else:
+            discount = self.promotion.discount(self)
+        return self.total() - discount
+
+class Promotion(ABC):
+    @abstractmethod
+    def discount(self,order:Order)->Decimal:
+        """Return Discount as Positive Dollar Amount"""
+
+class LoyaltyPromotion(Promotion):
+    """5 % discount if fidelity exceeds 1000 points"""
+    def discount(self,order:Order)->Decimal:
+        rate = Decimal('0.05')
+
+        if order.customer.fidelity >=1000:
+            return order.total()*rate
+        return Decimal(0)
+
+
+
+class WholeSalePromotion(Promotion):
+    """10% discount if order quantity exceeds 20"""
+    def discount(self, order: Order) -> Decimal:
+        discount = Decimal(0)
+        for item in order.cart:
+            if item.quantity>=20:
+                discount += item.total()*Decimal('0.1')
+        return discount
