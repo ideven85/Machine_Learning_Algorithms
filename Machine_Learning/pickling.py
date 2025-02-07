96a9f534828f91bcafaf29226f6ab387cad4d5e7
Deven Kalra <deven@Macbook.local>
2025-02-07 07:20:39 +0530

    Made Changes

--- Machine_Learning/pickling.py
+++ Machine_Learning/pickling.py
@@ -7,21 +7,34 @@ class Person:
     name: str
     age: int
 
+#@dataclass
+class Student(Person): # Not a data class unless decorated with it
+    grade: int
+
+
+
 def save_model():
     dave = Person("Deven",12)
     ser = pickle.dumps(dave)
-    with open('dave.pkl','wb') as f:
+    file = 'dave.pkl'
+    with open(file,'wb') as f:
         f.write(ser)
-    return ser
+    return file
 
-def load_model(model):
+
+
+def load_model(file):
+    with open(file,'rb') as f:
+        model = f.read()
     dave = pickle.loads(model)
     print(dave.name) # Side Effect not pure functional programming
     print(dave.age)
 
 def main():
-    model = save_model()
-    load_model(model)
+    #file=save_model()
+    #load_model(file)
+    stu = Student('Deven',12)
+    print(stu)
 
 
 if __name__ == '__main__':
