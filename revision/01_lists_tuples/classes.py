500fb727287e99e5617ab990035ac0f6c1131742
Deven Kalra <deven@Macbook.local>
2025-02-09 00:15:13 +0530

    Made changes

---
+++ revision/01_lists_tuples/classes.py
@@ -0,0 +1,18 @@
+class Demo:
+    @classmethod
+    def hello(*args):
+        return args
+
+    @staticmethod
+    def hello_static(*args):
+        return args
+
+def main():
+    print(Demo.hello('class')) # (<class '__main__.Demo'>, 'class')
+
+    print("static",Demo.hello_static,"class",Demo.hello) # static <function Demo.hello_static at 0x1043ce520> class <bound method Demo.hello of <class '__main__.Demo'>>
+
+    print(Demo.hello_static('class'))
+
+if __name__ == '__main__':
+    main()
---
+++ revision/01_lists_tuples/scoping.py
@@ -0,0 +1,8 @@
+fns=[]
+for i in range(5):
+    def func(x):
+        return x+i
+    fns.append(func)
+
+for f in fns:
+    print(f(12),i)# if you use i here it is still in play with value 4
