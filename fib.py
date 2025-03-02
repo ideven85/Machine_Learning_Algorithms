ccf0aad070e0bf6fae82437d0a03052cbb61b536
Deven Kalra <deven@Macbook.local>
2025-03-02 23:56:20 +0530

    Made Changes

---
+++ fib.py
@@ -0,0 +1,16 @@
+# fib.py
+
+import os
+import threading
+
+def fib(n):
+    return n if n < 2 else fib(n - 2) + fib(n - 1)
+# numpy_threads.py
+
+import numpy as np
+
+rng = np.random.default_rng()
+matrix = rng.random(size=(50000, 50000))
+matrix @ matrix
+# for _ in range(os.cpu_count()):
+#     threading.Thread(target=fib, args=(35,)).start()
