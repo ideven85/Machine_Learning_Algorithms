c1c008e3db408139b412f4c7945966c90f291f9d
Deven Kalra <deven@Macbook.local>
2026-03-08 21:56:48 +0530

    Made Changes

--- 6_101/Week3/reading3.py
+++ 6_101/Week3/reading3.py
@@ -16,7 +16,7 @@ def first_occurrence(data):
     Given a list of integers or strings, return a new list with the same
     set of items in the same order, but keeping only the first occurrence of
     each item.
-    >>>first_occurrence([1, 9, 1, 1, 5, 3, 2, 9, 10]) == [1, 9, 5, 3, 2, 10]
+    >>> first_occurrence([1, 9, 1, 1, 5, 3, 2, 9, 10]) == [1, 9, 5, 3, 2, 10]
     True
     """
     visited = set()
@@ -31,6 +31,31 @@ def first_occurrence(data):
 
 
 import doctest
+
+def can_log(item):
+    """
+    Check if can log
+
+    """
+    if isinstance(item,(str,bool,float)):
+        return True
+    elif isinstance(item,(list,tuple,set,frozenset)):
+        return all(can_log(x) for x in item)
+    elif isinstance(item,dict):
+        return all(can_log(x) and can_log(v) for x,v in item.values())
+    return False
+
+
+def sum_list(x):
+    """
+    Sums list
+    >>> sum_list([1,2]) == 3
+    """
+    if not x:
+        return 0
+    else:
+        return x[0]+sum_list(x[1:])
+
 
 if __name__ == "__main__":
     print(doctest.testmod(verbose=True))
