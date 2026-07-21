5998d06c144580df29a8d5175ca9c5bc01a10b15
Deven Kalra <deven@Macbook.local>
2026-07-21 22:06:28 +0530

    Till llm

--- Machine_Learning/opencv_course/__init__.py
+++
@@ -1,2 +0,0 @@
-# Time to put 8 hours a day
-# Will take 3 months... Both NLP and OpenCV... Target Date 31st January..
--- Machine_Learning/opencv_course/lesson1/writing_images.py
+++
@@ -1,4 +0,0 @@
-import cv2
-
-cat = cv2.imread("images/cat.png")
-cv2.imwrite("images/cat.png", cat)
